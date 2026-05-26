a5d09ac800f700e4fb8fe942c6f8fe364e4dbc23
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-05-26 11:39:14 +0530

    same Update in Codebase Done

--- 2.Varibles/Demo.java
+++ 2.Varibles/Demo.java
@@ -1,5 +1,12 @@
 public class Demo {
     public static void main(String[] args) {
-        // Intergers
+        // Intergers --> bytes, short, int, long
+        byte b = 6;
+        short s = 70;
+        int i = 6000;
+        long l = 200000;
+
+        System.out.println( "Interger values -->"+ b + " , "+ s + " , " + i + " , "+ l );
+
     }
 }
