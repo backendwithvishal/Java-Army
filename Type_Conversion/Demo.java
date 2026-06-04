349a20181dd0970c5953c1ffd4f76c9acfd110a2
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-04 20:28:35 +0530

    Type_Conversion folder added with implicit conversion

---
+++ Type_Conversion/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        // Implicit conversion
+        // (byte to int)
+        byte b = 56;
+        int i;
+
+        i = b;
+        System.out.println(i); //56
+    }
+}
