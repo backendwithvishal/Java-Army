8a9a7d5afad06f612b7f91257fed91e1db717c20
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-04 20:32:04 +0530

    Type_Conversion folder added with implicit conversion ( Char to int)

--- Type_Conversion/Demo.java
+++ Type_Conversion/Demo.java
@@ -2,10 +2,18 @@ public class Demo {
     public static void main(String[] args) {
         // Implicit conversion
         // (byte to int)
-        byte b = 56;
-        int i;
+        // byte b = 56;
+        // int i;
 
-        i = b;
-        System.out.println(i); //56
+        // i = b;
+        // System.out.println(i); //56
+
+        // Char to int
+        // char c = 's';
+        // int i;
+        
+        // i = c;
+
+        // System.out.println(i); // 115
     }
 }
