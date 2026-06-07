33dc6cd993edf48da2f9352d383f03ff1db694bd
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-07 19:13:42 +0530

    small update in logical opertors file

--- 4.Operators/Demo4.java
+++ 4.Operators/Demo4.java
@@ -1,3 +1,14 @@
 public class Demo4 {
-    
+    public static void main(String[] args) {
+        // Logical opertors
+        int a = 29;
+        int b = 10;
+        int c = 18;
+
+        boolean d = (a < b) && (b < c);
+
+        // short cricuit
+        
+        System.err.println(d);
+    }
 }
