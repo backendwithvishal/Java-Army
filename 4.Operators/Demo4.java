c0da0562ba0384921cc275cffa00f5dd276f6c08
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-07 18:56:43 +0530

    update in bitwise opertor file

--- 4.Operators/Demo4.java
+++ 4.Operators/Demo4.java
@@ -1,3 +1,16 @@
 public class Demo4 {
-    
+       public static void main(String[] args) {
+       // Bitwise operations
+       int a = 2; // 10 --> 00000000 00000000 00000000 00000010
+       int b = 3; // 11 --> 00000000 00000000 00000000 00000011
+       int c = a & b; // 10 --> 2
+       int d = a | b; // 11 --> 3
+       int e = a ^ b; // 01 --> 1
+       int f = ~a; // 11111111 11111111 11111111 11111101 --> -3
+
+       System.out.println(c + " , " + d + " , " + e + " , " + f);
+
+       //Shift opertions 
+       
+    }
 }
