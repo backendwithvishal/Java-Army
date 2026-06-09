624a8a2014075eadf05ce9c88c5b86e691f6f3f5
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-09 20:44:41 +0530

    if-else-if ladder code added in condition statement

--- 5.Conditional Statements/Demo.java
+++ 5.Conditional Statements/Demo.java
@@ -2,13 +2,16 @@
 public class Demo {
 
     public static void main(String[] args) {
-        boolean b = true;
-        int i = 5;
+        // boolean b = true;
+        // int i = 5;
+
+
         // Selection statements
         // Normal if 
         // if (i > 5) {
         //     System.out.println("The number is greater than 5.");
         // }
+
 
         // if else 
         // if (i > 5 && i < 10) {
@@ -16,13 +19,14 @@ public class Demo {
         // } else {
         //     System.out.println("i is less then or equal to 5");
         // }
-        if (i % 2 == 0) {
-            if (i > 5 && i < 10) {
-                System.err.println("i is odd");
-            }
-        } else {
-            System.out.println("i is even");
-        }
+        // if (i % 2 == 0) {
+        //     if (i > 5 && i < 10) {
+        //         System.err.println("i is odd");
+        //     }
+        // } else {
+        //     System.out.println("i is even");
+        // }
+
 
         // nested if
         // i = 8
@@ -36,5 +40,28 @@ public class Demo {
         // }
         // else {
         // }
+
+
+        // if-else-if ladder
+        // boolean b = true;
+        // int i = 7;
+        // // if-else-if ladder
+        // if(i == 5) {
+        //     System.out.println("i is 5");
+        // }
+        // else if(i == 6) {
+        //     System.out.println("i is 6");
+        // }
+        // else if(i == 7) {
+        //     System.out.println("i is 7");
+        // }
+        // else if(i == 8) {
+        //     System.out.println("i is 8");
+        // }
+        // else if(i == 9) {
+        //     System.out.println("i is 9");
+        // }
+
+        
     }
 }
