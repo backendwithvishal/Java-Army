50b6659638e7e5769f09f2cc58ddff2f58c75e5e
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-09 20:32:19 +0530

    first Condition statement added

---
+++ 5.Conditional Statements/Demo.java
@@ -0,0 +1,21 @@
+public class Demo {
+    public static void main(String[] args) {
+        boolean b = true;
+        int i = 5;
+        // Selection statements
+        // Normal if 
+        // if (i > 5 && i < 10) {
+        //     System.err.println("i is greater then5");
+        // } else {
+        //     System.out.println("i is less then or equal to 5");
+        // }
+
+        if (i % 2 == 0) {
+            if (i > 5 && i < 10) {
+                System.err.println("i is odd");
+            }
+        } else {
+            System.out.println("i is even");
+        }
+    }
+}
