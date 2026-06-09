890135c53940b92e2ac0dfb0d44ce3174f4824d4
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-09 21:20:03 +0530

    switch case code added in Demo 2 file

--- 5.Conditional Statements/Demo2.java
+++ 5.Conditional Statements/Demo2.java
@@ -1,3 +1,28 @@
+
 public class Demo2 {
-    
+
+    public static void main(String[] args) {
+        // switch statements
+        int i = 3;
+
+        switch (i) { // switch(expression) --> byte, short, int, char, Enumeration
+            case 1:
+                System.out.println("i is 1");
+                break;
+            case 2:
+                System.out.println("i is 2");
+                break;
+            case 3:
+                System.out.println("i is 3");
+                break;
+            default:
+                System.out.println("i is greater then 3 or more");
+                break;
+        }
+
+        // switch --> limited use case -> should evaluate to byte, short, int, char, Enumeration.
+        // No duplicate cases allowed.
+        // After JDK7 Strings can also be used as switch expression. 
+        // After JDK-14 switch is enhanced too much.
+    }
 }
