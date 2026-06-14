35e4d5dc6843cac7c8fa69066385fabcc3f36d8b
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-14 20:49:43 +0530

    do while loop added

--- 7.Loop&JumpStatements/Demo.java
+++ 7.Loop&JumpStatements/Demo.java
@@ -2,12 +2,18 @@
 public class Demo {
 
     public static void main(String[] args) {
-        int v = 0; //1 to 10
+        int v = 10; //1 to 10
 
-// While Loop
-        while (v++ < 10) { // boolean --> true / fasle}
+        // While Loop
+        // while (v++ < 10) { // boolean --> true / fasle}
+        //     System.err.println(v);
+        //     v++;
+        // }
+
+        // do while Loop
+        do {
             System.err.println(v);
             v++;
-        }
+        } while(v <= 10);
     }
 }
