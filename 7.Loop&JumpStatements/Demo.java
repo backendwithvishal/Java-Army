e82bf01883135c6c067a55909dbb122a5d114d42
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-14 20:42:31 +0530

    while loop code added

--- 7.Loop&JumpStatements/Demo.java
+++ 7.Loop&JumpStatements/Demo.java
@@ -1,5 +1,10 @@
 public class Demo {
+
     public static void main(String[] args) {
-        
+        int v = 0; //1 to 10
+
+        while (v++ < 10) { // boolean --> true / fasle}
+            System.err.println(v);
+        }
     }
 }
