143a2c0ef98998cedd9e5998ddbd5e4550afa123
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-14 20:44:41 +0530

    small update in while loop

--- 7.Loop&JumpStatements/Demo.java
+++ 7.Loop&JumpStatements/Demo.java
@@ -1,10 +1,13 @@
+
 public class Demo {
 
     public static void main(String[] args) {
         int v = 0; //1 to 10
 
+// While Loop
         while (v++ < 10) { // boolean --> true / fasle}
             System.err.println(v);
+            v++;
         }
     }
 }
