e084cf52cd0038f50239f0a7fd497e76d598054d
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-15 17:12:08 +0530

    For loop added

--- 7.Loop&JumpStatements/Demo.java
+++ 7.Loop&JumpStatements/Demo.java
@@ -2,8 +2,8 @@
 public class Demo {
 
     public static void main(String[] args) {
-        int v = 10; //1 to 10
-
+        //int v = 10; //1 to 10
+        // int v
         // While Loop
         // while (v++ < 10) { // boolean --> true / fasle}
         //     System.err.println(v);
@@ -11,9 +11,25 @@ public class Demo {
         // }
 
         // do while Loop
-        do {
-            System.err.println(v);
-            v++;
-        } while(v <= 10);
+        // do {
+        //     System.err.println(v);
+        //     v++;
+        // } while(v <= 10);
+
+        // For Loop
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        /* 
+        FLow of control of for
+         1. First assignment statement is executed (Variable definition).
+         2. Then second conditional statement is evaluated. (True / false)
+         3. If true, control flow will evaluate the body of the loop.
+         4. Once loop body is finished, control flow will go back to the for statement, and ithird increment
+         statement will be evaluated.
+         5. Again, conditional statement is evaluated.
+         6. Repeat 2 - 5.
+        */
     }
 }
