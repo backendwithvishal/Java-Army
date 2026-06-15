b76b1067e17d12297c2ca5fe233b811a23eff5e0
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-15 21:07:43 +0530

    Nested loop with start pattern

--- 7.Loop&JumpStatements/Loop2.java
+++ 7.Loop&JumpStatements/Loop2.java
@@ -1,5 +1,11 @@
 public class Loop2 {
     public static void main(String[] args) {
-        
+        // Nested loops with start pattern
+        for (int v = 1; v <= 5; v++){
+            for (int s = 1; s <= v; s++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
