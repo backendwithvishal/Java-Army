8e67be2846012494cfc700011a764faa0ac201b9
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-15 21:12:11 +0530

    Java Jump Statement

--- 7.Loop&JumpStatements/Loop2.java
+++ 7.Loop&JumpStatements/Loop2.java
@@ -1,11 +1,17 @@
 public class Loop2 {
     public static void main(String[] args) {
         // Nested loops with start pattern
-        for (int v = 1; v <= 15; v++){
-            for (int s = 1; s <= v; s++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        // for (int v = 1; v <= 15; v++){
+        //     for (int s = 1; s <= v; s++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        //Jump Statement in Java
+        // break,, continue,, return
+
+        
     }
 }
+
