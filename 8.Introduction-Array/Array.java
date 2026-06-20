2b7b883d4016d994ae4c18a2135ba536c432109c
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-20 22:57:49 +0530

    java array code added

---
+++ 8.Introduction-Array/Array.java
@@ -0,0 +1,16 @@
+public class Array{
+    public static void main(String[] args){
+        int[] rollNo = new int[3];
+        //101, 102, 103
+
+        rollNo[0] = 101;
+        rollNo[1] = 102;
+        rollNo[2] = 103;
+
+        System.out.println(rollNo[0]);
+        System.out.println(rollNo[1]);
+        System.out.println(rollNo[2]);
+
+        System.out.println(rollNo.length);
+    }
+}
