298acf603af09255f1c42807fd9716e4cea26112
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-20 23:05:50 +0530

    comment added in codebase

--- 8.Introduction-Array/Array.java
+++ 8.Introduction-Array/Array.java
@@ -1,5 +1,8 @@
-public class Array{
-    public static void main(String[] args){
+// Simple program to demonstrate array initialization and iteration
+public class Array {
+    // Main method: program entry point
+    public static void main(String[] args) {
+        // Declare an integer array of size 3
         int[] rollNo = new int[3];
         //101, 102, 103
 
@@ -14,12 +17,13 @@ public class Array{
         // System.out.println(rollNo.length);
 
         int v = 101;
-        for(int s = 0; s < rollNo.length; s++){
+        // Fill the array with sequential values
+        for (int s = 0; s < rollNo.length; s++) {
             rollNo[s] = v;
             v++;
         }
-
-        for(int s = 0; s < rollNo.length; s++){
+        // Print each element of the array
+        for (int s = 0; s < rollNo.length; s++) {
             System.out.println(rollNo[s]);
         }
     }
