532f3083612cb0a38f0aa14b88a7f8d14237f4de
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-21 07:48:01 +0530

    code added in new file

---
+++ 8.Introduction-Array/Array2.java
@@ -0,0 +1,10 @@
+public class Array2 {
+    public static void main(String[] args) {
+        // String 
+        String firstName = "Vishal";
+        String lastName = "Sanam";
+
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
