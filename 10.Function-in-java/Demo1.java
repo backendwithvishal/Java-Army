88dc136600d919a1873b544f7b0e34725256c2c3
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-22 12:34:43 +0530

    Code added

--- 10.Function-in-java/Demo1.java
+++ 10.Function-in-java/Demo1.java
@@ -4,10 +4,20 @@ public class Demo1 {
         // Same name , Different parameters
 
         System.out.println(sum(1,2));
-        
+
         System.out.println(sum(1,2,3));
 
         System.out.println(sum(1.0,2.0));
+
+        name();
+
+        name("Vishal");
+
+        name("Vishal", 22);
+
+        name("Vishal", 22, "Thane");
+
+        name("Vishal", 22, "Thane", "Maharashtra");
     }
 
     static int sum(int a, int b) {
@@ -20,5 +30,25 @@ public class Demo1 {
 
     static int sum(double a, double b) { // different data type of parameters
         return (int)(a + b);
+    }
+    
+    static void name() {
+        System.out.println("Hello World");
+    }
+    
+    static void name(String name) {
+        System.out.println("Hello " + name);
+    }
+    
+    static void name(String name, int age) {
+        System.out.println("Hello " + name + ", you are " + age + " years old");
+    }
+
+    static void name(String name, int age, String city) {
+        System.out.println("Hello " + name + ", you are " + age + " years old and live in " + city);
+    }
+
+    static void name(String name, int age, String city, String state) {
+        System.out.println("Hello " + name + ", you are " + age + " years old and live in " + city + ", " + state);
     }
 }
