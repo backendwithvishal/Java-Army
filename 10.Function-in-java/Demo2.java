a0d2105b69bce445c475ed1498782049d83213a0
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-22 12:43:42 +0530

    new file added with code

---
+++ 10.Function-in-java/Demo2.java
@@ -0,0 +1,21 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        // function calling
+
+        fun1();
+
+        System.out.println("Bye");
+    }
+
+    static void fun1() {
+        System.out.println("Hiiii"); // 1st
+    }
+
+    static void fun2() {
+        System.out.println("Heello"); // 2nd
+    }
+
+    static void fun3() {
+        System.out.println("How are u "); // 3rd
+    }
+}
