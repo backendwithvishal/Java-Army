c5b088e380a79d872c5354860936a9b13067f92a
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-22 12:09:45 +0530

    Code added in file

---
+++ 10.Function-in-java/Demo.java
@@ -0,0 +1,31 @@
+public class Demo {
+    public static void main(String[] args) {
+        // Function in java
+
+    }
+
+    // No input , No output
+    static void greet() {
+        System.out.println("Hello World");
+    }
+
+    // Input , No Output
+    static void SayHello(String name){
+        System.out.println("Hello " + name);
+    }
+
+    // No Input , Output
+    static int Sum(){
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        return c;
+    }
+
+    // Input , Output
+    static int Sum(int a , int b){
+        int c = a + b;
+        return c;
+    }
+
+}
