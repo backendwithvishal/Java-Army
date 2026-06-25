cf6ffa5ad1fc480b9c2b19847dc2b7fafe751186
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-25 20:51:13 +0530

    code added in file

---
+++ 11.OOPS/Demo.java
@@ -0,0 +1,41 @@
+public class Demo {
+    public static void main(String[] args) {
+        // System.out.println("Hello World");
+
+        // Student s1 = new Student();
+        s1.name = "Vishal";
+        s1.age = 20;
+        s1.rollNumber = 1;
+        s1.college = "Sahyog College";
+
+        // Student s2 = new Student();
+        s2.name = "Shravani";
+        s2.age = 19;
+        s2.rollNumber = 2;
+        s2.college = "Sahyog College";
+
+        s1.markAttendance();
+        s2.markAttendance();
+
+        s1.print();
+        s2.print();
+    }
+
+    class Student {
+        String name;
+        int age;
+        int rollNumber;
+        String college;
+
+        void markAttendance() {
+            System.out.println("Attendance marked for " + name);
+        }
+
+        void print() {
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+            System.out.println("Roll Number: " + rollNumber);
+            System.out.println("College: " + college);
+        }
+    }
+}
