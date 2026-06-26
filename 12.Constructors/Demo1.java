9a40350e162a45847168e7ba38845259e687d57d
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-27 00:53:17 +0530

    code added in Demo1.java file

---
+++ 12.Constructors/Demo1.java
@@ -0,0 +1,16 @@
+public class Demo1{
+    public static void main(String[] args) {
+        
+
+    }
+}
+
+class Student {
+    String name;
+    int rollNo;
+    double cgpa;
+
+    void markAttendence() {
+        System.out.println(name + " marked attendence");
+    }
+}
