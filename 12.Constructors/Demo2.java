80bde5cb1ad84337cfe225efdb3c114e69d76ee8
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-06-27 20:27:38 +0530

    This Keyword & Chaining

--- 12.Constructors/Demo2.java
+++ 12.Constructors/Demo2.java
@@ -1,1 +1,53 @@
-// This Keyword
+// This Keyword & Chaining
+
+public class Demo1{
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s1 = new Student("Vishal");
+        Student s3 = new Student("Vishal", 22);
+        Student s2 = new Student("Vishal", 21, 9.0);
+
+        System.out.println(s1.name);
+        System.out.println(s1.rollNo);
+        System.out.println(s1.cgpa);
+    }
+}
+
+class Student {
+    String name;
+    int rollNo;
+    double cgpa;
+    
+    Student() {
+        System.out.println("constructor is calling with this keyword");
+    }
+
+    // Parameterized constructor with one parameterized this Keyword
+    Student(String name) {
+        this(name);
+        
+    }
+
+    // Two Parameterized constructor with two parameterized this Keyword
+    Student(String name, int rollNo) {
+        this(name, rollNo);
+    }
+
+    // Three Parameterized constructor with this Keyword
+    Student(String name, int rollNo, double cgpa) {
+        this(name, rollNo, cgpa);
+    }
+
+    // Parameterized constructor with this Keyword
+    Student(String name, int rollNo, double cgpa) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.cgpa = cgpa;
+    }
+    
+
+
+    void markAttendence() {
+        System.out.println(name + "marked attendence");
+    }
+}
