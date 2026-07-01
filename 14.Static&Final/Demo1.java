0eb131aa1a8fe786861f40b5f863c572ee00f171
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-01 07:43:57 +0530

    code added in new file

---
+++ 14.Static&Final/Demo1.java
@@ -0,0 +1,20 @@
+// Final keyword
+public class Demo1{
+    public static void main(String args[]) {
+        Random r1 = new Random();
+        System.out.println(r1.PI);
+
+        final int X;
+        x = 4;
+        System.out.println(x);
+
+    }
+}
+
+class Random {
+    final double;
+
+    Random(){
+        this.PI = 3.14;
+    }
+}
