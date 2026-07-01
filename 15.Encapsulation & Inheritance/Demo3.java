b613ff12381317271845f59079d59956c809c7a1
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-01 21:33:54 +0530

    small update in Demo3 file

--- 15.Encapsulation & Inheritance/Demo3.java
+++ 15.Encapsulation & Inheritance/Demo3.java
@@ -3,3 +3,24 @@
 // Multi-level Inheritance
 // Hybrid Inheritance 
 // Multiple Inheritance
+
+public class Demo3 {
+    public static void main (String [] args){
+
+    }
+}
+
+class Student {  // Parent Class
+    String name;
+    int age;
+
+    void markAttendance() {
+        System.out.println("Attendance marked");
+    }
+}
+
+class EngineeringStudent extends Student {  // Child Class
+    void attendLab() {
+        System.out.println("lab attended");
+    }
+}
