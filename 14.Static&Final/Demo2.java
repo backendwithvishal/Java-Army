5f1e0f02d43f0a7419c63c2c9106a5ea532ede25
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-01 07:52:18 +0530

    new file added with code

---
+++ 14.Static&Final/Demo2.java
@@ -0,0 +1,9 @@
+public class Demo2{
+    public static void main(String args[]) {
+        System.out.println("Number of arguments are : " + args.length);
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + i + " = " + args[i]);
+        }
+    }
+}
