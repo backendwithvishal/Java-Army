1874002e6a3429e53661528a5bfe3b7a6302a9f8
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-01 21:03:43 +0530

    image added

--- 15.Encapsulation & Inheritance/Demo1.java
+++ 15.Encapsulation & Inheritance/Demo1.java
@@ -1,11 +1,11 @@
-import collage.Student;
+// import collage.Student;
 // import school.student;
 
 //import student.college;
 
 public class Demo1 {
     public static void main(String[] args) {
-        Student s1 = new Student();
+        collage.Student s1 = new collage.Student();
         s1.print(); 
 
         school.Student s2 = new school.Student();
