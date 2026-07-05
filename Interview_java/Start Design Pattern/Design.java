552c22b7caac0ed98befbdff1b749a2d8cb2ccc5
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-05 19:13:01 +0530

    small update in codebase

--- Interview_java/Start Design Pattern/Design.java
+++ Interview_java/Start Design Pattern/Design.java
@@ -4,48 +4,48 @@ public class Design {
     public static void main(String[] args) {
         // Square star pattern
 
-        // for (int i = 1; i < 5; i++) {
-        //     for (int j = 1; j <= 5; j++) {
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
         // Right Traingle
 
-        // for (int i = 1 ; i <=5; i++ ){
-        //     for(int j = 1 ; j <= i; j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for (int i = 1 ; i <=5; i++ ){
+            for(int j = 1 ; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
         // Repeating number traingle
 
-        // for (int i = 1 ; i <=5; i++ ){
-        //     for(int j = 1 ; j <= i; j++){
-        //         System.out.print(i + " ");
-        //     }
-        //     System.out.println();
-        // }
+        for (int i = 1 ; i <=5; i++ ){
+            for(int j = 1 ; j <= i; j++){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
 
         // Inverted Right Traingle
 
-        // for (int i = 4; i >= 1; i--) {
-        //     for (int j = 1; j <= i; j++) {
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
         // Number Traingle
 
-        // for (int i = 1 ; i <=5; i++ ){
-        //     for(int j = 1 ; j <= i; j++){
-        //         System.out.print(j + " ");
-        //     }
-        //     System.out.println();
-        // }
+        for (int i = 1 ; i <=5; i++ ){
+            for(int j = 1 ; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
 
         // Full Pyrmaid
 
