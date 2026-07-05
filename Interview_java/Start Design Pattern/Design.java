5f04d2619299f70a7f1291b9973ebb6873555674
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-05 19:11:10 +0530

    full pyrmaid Code added

--- Interview_java/Start Design Pattern/Design.java
+++ Interview_java/Start Design Pattern/Design.java
@@ -10,33 +10,57 @@ public class Design {
         //     }
         //     System.out.println();
         // }
+
         // Right Traingle
+
         // for (int i = 1 ; i <=5; i++ ){
         //     for(int j = 1 ; j <= i; j++){
         //         System.out.print("*");
         //     }
         //     System.out.println();
         // }
+
         // Repeating number traingle
+
         // for (int i = 1 ; i <=5; i++ ){
         //     for(int j = 1 ; j <= i; j++){
         //         System.out.print(i + " ");
         //     }
         //     System.out.println();
         // }
+
         // Inverted Right Traingle
+
         // for (int i = 4; i >= 1; i--) {
         //     for (int j = 1; j <= i; j++) {
         //         System.out.print("*");
         //     }
         //     System.out.println();
         // }
-        
+
         // Number Traingle
-        for (int i = 1 ; i <=5; i++ ){
-            for(int j = 1 ; j <= i; j++){
-                System.out.print(j + " ");
+
+        // for (int i = 1 ; i <=5; i++ ){
+        //     for(int j = 1 ; j <= i; j++){
+        //         System.out.print(j + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Full Pyrmaid
+
+        for (int i = 1; i <= 5; i++) {
+
+            // Print spaces
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
             }
+
+            // Print stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
             System.out.println();
         }
     }
