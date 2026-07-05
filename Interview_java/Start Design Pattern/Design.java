b324fd63bb90af6852a2b6467e8d5916a9776abe
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-05 18:26:52 +0530

    Small updare in codebase

--- Interview_java/Start Design Pattern/Design.java
+++ Interview_java/Start Design Pattern/Design.java
@@ -3,11 +3,28 @@ public class Design {
     public static void main(String[] args) {
         // Square star pattern
 
-        for (int i = 1; i < 5; i++) {
-            for (int j = 1; j <= 5; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        // for (int i = 1; i < 5; i++) {
+        //     for (int j = 1; j <= 5; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Right Traingle
+
+        // for (int i = 1 ; i <=5; i++ ){
+        //     for(int j = 1 ; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // Right angle Traigle with number
+        // for (int i = 1 ; i <=5; i++ ){
+        //     for(int j = 1 ; j <= i; j++){
+        //         System.out.print(i + " ");
+        //     }
+        //     System.out.println();
+        // }
     }
 }
