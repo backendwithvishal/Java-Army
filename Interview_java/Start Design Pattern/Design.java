28d227a9ad30ef3d4a030e9f37a03285e2fbf886
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-05 18:16:58 +0530

    degining star code added

---
+++ Interview_java/Start Design Pattern/Design.java
@@ -0,0 +1,13 @@
+public class Design {
+
+    public static void main(String[] args) {
+        // Square star pattern
+
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
