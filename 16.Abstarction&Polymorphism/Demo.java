54a81aeff93c64bc6bd388bb4daee81ef26a2bea
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-15 20:12:17 +0530

    code added in file

--- 16.Abstarction&Polymorphism/Demo.java
+++ 16.Abstarction&Polymorphism/Demo.java
@@ -1,5 +1,19 @@
 public class Demo{
     public static void main (String[] args) {
-        System.out.println("Start");
+        System.out.println();
     }
+}
+
+class Car {
+    void start(){
+        System.outt.println("Car Started")
+    }
+}
+
+void accelerate(){
+    System.outt.println("Car is accelarting at 60km/h")
+}
+
+void break(){
+    System.outt.println("Car Stoped")
 }
