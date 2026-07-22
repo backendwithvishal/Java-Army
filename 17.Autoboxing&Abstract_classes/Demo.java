0db23ee604a22bbc15109573858a684239d1322b
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-22 07:38:25 +0530

    code added in new file

---
+++ 17.Autoboxing&Abstract_classes/Demo.java
@@ -0,0 +1,40 @@
+public class Demo{
+    public static void main(String[] args){
+        // // Autoboxing
+        // int v = 45;
+        // Integer s = v; 
+
+        // System.out.println(v);
+        // System.out.println(s);
+
+        // // Unboxing
+        // Integer a = 7;
+        // int b = a;
+
+        // System.out.println(a);
+        // System.out.println(b);
+
+        // int x = 93;
+        // printInteger(x);
+
+        // Integer a = 18;
+        // Integer b = 96;
+
+        // int sum = a.intValue() + b.intValue();
+        // System.out.println(sum);
+
+        Integer x = null;
+        int y = x;
+        System.out.println(y);
+
+
+    }
+
+    static void printInteger(Integer x) {
+        System.out.println(x);
+    }
+}
+
+// 1. Assignments
+// 2. Method calls
+// 3. Arithmetic operations
