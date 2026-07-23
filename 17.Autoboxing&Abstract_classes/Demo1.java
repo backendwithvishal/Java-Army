79278f8ffd28d0bf692abf75e8256145eabf5017
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-23 11:58:41 +0530

    comment added in code

--- 17.Autoboxing&Abstract_classes/Demo1.java
+++ 17.Autoboxing&Abstract_classes/Demo1.java
@@ -9,18 +9,31 @@ public class Demo1{
         // System.out.println(a == b); 
 
         // Autoboxing 
-        int x = 10;
-        Integer y = x; // autoboxing
+        // int x = 10;
+        // Integer y = x; // autoboxing
 
-        System.out.println(x);
-        System.out.println(y);  // unboxing
+        // System.out.println(x);
+        // System.out.println(y);  // unboxing
 
-        //Unboxing
-        Integer a = 22; // Autoboxing
-        int b = a; // unboxing
+        // //Unboxing
+        // Integer a = 22; // Autoboxing
+        // int b = a; // unboxing
 
-        System.out.println(a); 
-        System.out.println(b); 
+        // System.out.println(a); 
+        // System.out.println(b); 
+
+        // int x = 50;
+        // printInteger(x); // auto boxing
+
+        Integer a = 10; 
+        Integer b = 20; // auto boxing
+
+        int sum = a.intValue() + b.intValue(); // unboxing
+        System.out.println(sum);  // auto boxing
+    }
+
+    static void printInteger(Integer x) {
+        System.out.println(x); // auto unboxing
 
     }
 }
