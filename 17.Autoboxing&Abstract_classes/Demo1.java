56b6ad7fed069b8a33658ec59c398e1530e69d30
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-23 11:51:47 +0530

    new code added in file

--- 17.Autoboxing&Abstract_classes/Demo1.java
+++ 17.Autoboxing&Abstract_classes/Demo1.java
@@ -4,8 +4,23 @@ public class Demo1{
         // int y = 20;
         // System.out.println(x == y);
 
-        Integer a = 22;
-        Integer b = 22;
-        System.out.println(a == b);
+        // Integer a = 22;
+        // Integer b = 22;
+        // System.out.println(a == b); 
+
+        // Autoboxing 
+        int x = 10;
+        Integer y = x; // autoboxing
+
+        System.out.println(x);
+        System.out.println(y);  // unboxing
+
+        //Unboxing
+        Integer a = 22; // Autoboxing
+        int b = a; // unboxing
+
+        System.out.println(a); 
+        System.out.println(b); 
+
     }
 }
