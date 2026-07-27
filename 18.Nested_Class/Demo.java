a38ce4cb8789cb1f07b80c828a97da114fc49f0a
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-27 12:37:39 +0530

    code added in New file

--- 18.Nested_Class/Demo.java
+++ 18.Nested_Class/Demo.java
@@ -1,5 +1,32 @@
 public class Demo {
     public static void main (String [] args) {
+
+        Outer outer = new Outer();
+
+        Outer.Inner inner = new Outer.Inner(outer);
+        inner.fun();
+    }
+}
+
+class Outer {
+
+    static int x = 7;
+    int y;
+
+    static class Inner {
+
+        Outer outer;
+        String name;
+        static int number;
         
+
+        Inner(Outer outer) {
+            this.outer = outer;
+        }
+
+        void fun(Outer outer){
+            System.out.println(x);
+            System.out.println(outer.y);
+        }
     }
 }
