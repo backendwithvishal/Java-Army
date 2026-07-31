964a0be17b03b9276f88ec772f0cc9305eaa74a8
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-07-31 12:35:15 +0530

    Code added

---
+++ 19.User-Input/Demo2.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+public class Demo2 {
+    public static void main(String [] args) throws IOException{
+        int x = System.in.read();
+
+        System.out.println((char)x);
+    }
+}
