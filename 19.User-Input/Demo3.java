0d6aa3fbef2bef2591906691e9aa5de27a05a625
WebDevloperVishal <vishalsanam2024@gmail.com>
2026-08-01 21:33:12 +0530

    code added in new file

---
+++ 19.User-Input/Demo3.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class Demo3{
+    public static void main(String[] args) throws IOException {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        String name =  br.read.ine();
+
+        System.out.println(name);
+    }
+}
